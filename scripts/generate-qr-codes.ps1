d0313a4a0cec7a44525336910e0a1db908a4d174
Martian <tinus1.prinsloo@gmail.com>
2026-02-20 16:33:02 +0200

    (fea) Transparency added to qr-code

--- scripts/generate-qr-codes.ps1
+++ scripts/generate-qr-codes.ps1
@@ -2,7 +2,8 @@ param(
   [string]$Url = "https://bcwildwatch.co.za",
   [string]$Name = "public/BC Wild Watch QRCode",
   [string]$Logo = "public/BC Wild Watch Logo.svg",
-  [double]$LogoSize = 0.25
+  [double]$LogoSize = 0.25,
+  [switch]$IncludeBackground
 )
 
 if (!(Test-Path $Logo) -or !($Logo -like "*.svg")) {
@@ -19,40 +20,42 @@ $qr_svg_source = @"
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1">
   <defs>
-    <symbol id="qrCode">
-      <mask id="qrMask">
-        <image
-          width="1"
-          href="data:image/png;base64,$qr_png_base64"
-          style="image-rendering: pixelated" />
-      </mask>
-      <rect width="1" height="1" fill="currentColor" />
+    <symbol id="QrCode">
       <rect
         width="1"
         height="1"
         fill="currentColor"
-        mask="url(#qrMask)"
-        style="filter: invert(1)" />
+        mask="url(#QrDotsMask)" />
+      <mask id="QrDotsMask">
+        <image
+          width="1"
+          href="data:image/png;base64,$qr_png_base64"
+          style="image-rendering: pixelated; filter: invert(1)" />
+      </mask>
     </symbol>
-    <symbol id="logo" viewBox="0 0 100 100">
+    <symbol id="logo" viewBox="$($logo_svg_source -replace '[\s\S]*?<svg[\s\S]*?viewBox="([\d\s]+)"[\s\S]*>[\s\S]*','$1')">
     $(
-        ($logo_svg_source -replace '[\s\S]*<svg[\s\S]*?>([\s\S]*)</svg>[\s\S]*','$1'
+        ($logo_svg_source -replace '[\s\S]*?<svg[\s\S]*?>([\s\S]*)</svg>[\s\S]*','$1'
         ) -replace '\r?\n','
     ')
     </symbol>
   </defs>
+  $(if ($IncludeBackground) { '<rect width="1" height="1" fill="white" />' })
   <use
-    href="#qrCode"
+    href="#QrCode"
     x="0"
     y="0"
     width="1"
-    height="1" />
-  <circle
+    height="1"
+    mask="url(#logoCutOut)" />
+  <mask id="logoCutOut">
+    <rect width="1" height="1" fill="white" />
+    <circle
     cx="0.5"
     cy="0.5"
     r="$($LogoSize * 0.75)"
-    fill="currentColor"
-    style="filter: invert(1)" />
+    fill="black" />
+  </mask>
   <use
     href="#logo"
     x="$(0.5 - $LogoSize / 2.0)"
