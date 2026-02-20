38c81384d32b107af608e342a38b34e8c01677fa
Martian <tinus1.prinsloo@gmail.com>
2026-02-20 15:29:02 +0200

    (fea) Github pages deployment

--- scripts/generate-qr-codes.ps1
+++ scripts/generate-qr-codes.ps1
@@ -1,7 +1,7 @@
 param(
   [string]$Url = "https://bcwildwatch.co.za",
-  [string]$Name = "images/BC Wild Watch QRCode",
-  [string]$Logo = "images/BC Wild Watch Logo.svg",
+  [string]$Name = "public/BC Wild Watch QRCode",
+  [string]$Logo = "public/BC Wild Watch Logo.svg",
   [double]$LogoSize = 0.25
 )
 
